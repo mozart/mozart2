53491c1521903cde1082769aa1161413c3dc19de
Guillaume Maudoux <guillaume.maudoux@uclouvain.be>
2016-01-20 15:07:24 +0100

    Add a 'dist' target
    The 'dist' target builds a source archive that include the source files
    generated with the generator.
    Building from that source does not require LLVM nor Clang.

--- cmake_local/MozartProperties.cmake
+++ cmake_local/MozartProperties.cmake
@@ -14,40 +14,49 @@ endif()
 
 target_architecture(MOZART_PROP_PLATFORM_ARCH)
 
+# For source tarballs, oz.version and os.date
+# are exported id MozartConfigVersion.cmake
+include(MozartConfigVersion
+  RESULT_VARIABLE MOZART_CACHED_BUILD OPTIONAL)
+
 # oz.version
 
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} describe --dirty
-  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-  OUTPUT_VARIABLE git_describe_output
-  ERROR_QUIET
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
+if(NOT MOZART_CACHED_BUILD)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --dirty
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    OUTPUT_VARIABLE git_describe_output
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if("${git_describe_output}" MATCHES "^v[0-9].+-[0-9]+-g[0-9a-f]+(-dirty)?$")
-  string(REGEX REPLACE "^v(.+)-([0-9]+)-g([0-9a-f]+)((-dirty)?)$" "\\1+build.\\2.\\3\\4"
-         MOZART_PROP_OZ_VERSION "${git_describe_output}")
-elseif("${git_describe_output}" MATCHES "^v[0-9].+[0-9](-dirty)?$")
-  string(REGEX REPLACE "^v(.+)$" "\\1"
-         MOZART_PROP_OZ_VERSION "${git_describe_output}")
-else()
-  message(WARNING "'git describe --dirty' did not return something sensible")
-  set(MOZART_PROP_OZ_VERSION "unknown")
+  if("${git_describe_output}" MATCHES "^v[0-9].+-[0-9]+-g[0-9a-f]+(-dirty)?$")
+    string(REGEX REPLACE "^v(.+)-([0-9]+)-g([0-9a-f]+)((-dirty)?)$" "\\1+build.\\2.\\3\\4"
+           MOZART_PROP_OZ_VERSION "${git_describe_output}")
+  elseif("${git_describe_output}" MATCHES "^v[0-9].+[0-9](-dirty)?$")
+    string(REGEX REPLACE "^v(.+)$" "\\1"
+           MOZART_PROP_OZ_VERSION "${git_describe_output}")
+  else()
+    message(WARNING "'git describe --dirty' did not return something sensible")
+    set(MOZART_PROP_OZ_VERSION "unknown")
+  endif()
+
+  unset(git_describe_output)
 endif()
-
-unset(git_describe_output)
 
 # oz.date
 
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} log "--format=%aD" -1
-  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-  OUTPUT_VARIABLE git_date_output
-  ERROR_QUIET
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
+if(NOT MOZART_CACHED_BUILD)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} log "--format=%aD" -1
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    OUTPUT_VARIABLE git_date_output
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-set(MOZART_PROP_OZ_DATE "${git_date_output}")
+  set(MOZART_PROP_OZ_DATE "${git_date_output}")
 
-unset(git_date_output)
+  unset(git_date_output)
+endif()
 
 # Display which version we found
 
